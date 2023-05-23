31bea7316c96dd85d6b358b4ca88468e20fed40d
Vinicius Vaz <agvazvinicius@gmail.com>
2023-05-23 20:14:55 -0300

    renderização-condicional

---
+++ 1-meu-projeto-react/src/components/MeuComponente.jsx
@@ -0,0 +1,11 @@
+function MeuComponente() {
+	return (
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat.
+		</p>
+	);
+}
+
+export default MeuComponente
